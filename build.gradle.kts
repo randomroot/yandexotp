ebdfd28b92f04551ec35191a1284f1f912f0aa1e
Mikhail Prokofev <mprokofev@divcu.com>
2022-01-14 20:14:54 +0300

    Add Gradle project files

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("multiplatform") version "1.5.10"
+}
+
+group = "ru.mihailpro.lib"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js(BOTH) {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+    }
+}
